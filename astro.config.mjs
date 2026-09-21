e3a84abf45af79d7ccb981dfa8668b41ab12744e
rnoyer <romain.noyer@gmail.com>
2026-09-21 22:57:42 +0200

    feat: github page config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://rnoyer.github.io",
+  base: "/altitude",
   fonts: [
     {
       provider: fontProviders.fontsource(),
